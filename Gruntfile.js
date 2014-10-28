94d1325d20a70662e210bce3ceb6f9be68f8cc3b
rybka <rashan_soft@inbox.ru>
2014-10-28 23:05:21 +0200

    comment wrong config. Add new config: base, LVH

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,26 @@
 'use strict';
 module.exports = function(grunt) {
   var ls = require('./lsv1');
+  var basePath: './site/spriteFactory';
   grunt.initConfig({
     sprite: {
+      options: {
+        'cssTemplate': 'spritesmith-template.mustache',
+        'padding': 2,
+        'imgPath': '../images/sprite.png',
+        'cssFormat': 'less',
+      },
+      mobSiteLVH: {
+        'src': ['<%= basePath %>/LVH/_mobSite/images/*.png'],
+      },
+      webSiteLVH: {
+
+      },
+      paymentLVH: {
+
+      }
+    }
+    /*sprite: {
       mobSite: {
         src: ls._mobSite().imgSrc(),
         destImg: ls._mobSite().destImg(),
@@ -12,7 +30,8 @@ module.exports = function(grunt) {
         cssTemplate: 'spritesmith-template.mustache',
         cssFormat: 'less'
       }
-    }
+    }*/
+
   });
   grunt.loadNpmTasks('grunt-spritesmith');
   grunt.registerTask('default', ['sprite']);
