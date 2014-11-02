ecb21fe0418ec256d9497bab8612fea73f996200
rybka <rashan_soft@inbox.ru>
2014-11-02 13:33:47 +0200

    update config for Gruntfile.js. Use helpers for config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,12 @@
 'use strict';
 module.exports = function(grunt) {
-  var ls = require('./lsv1');
-  grunt.initConfig({
-    basePath: './site/spriteFactory',
-    sprite: {
-      mobSiteLVH: {
-        'cssTemplate': 'spritesmith-template.mustache',
-        'padding': 2,
-        'imgPath': '../images/sprite.png',
-        'cssFormat': 'less',
-        'algorithm': 'binary-tree',
-        'src': ['<%= basePath %>/LVH/_mobSite/images/*.png'],
-        'destImg': '<%= basePath %>/LVH/_mobSite/sprite/sprite.png',
-        'destCSS': '<%= basePath %>/LVH/_mobSite/sprite/sprite.less'
-      },
-      webSiteLVH: {
-        'cssTemplate': 'spritesmith-template.mustache',
-        'padding': 2,
-        'imgPath': '../images/sprite.png',
-        'cssFormat': 'less',
-        'algorithm': 'binary-tree',
-        'src': ['<%= basePath %>/LVH/_webSite/images/*.png'],
-        'destImg': '<%= basePath %>/LVH/_webSite/sprite/sprite.png',
-        'destCSS': '<%= basePath %>/LVH/_webSite/sprite/sprite.less'
-      },
-      payment: {
-        'cssTemplate': 'spritesmith-template.mustache',
-        'padding': 2,
-        'imgPath': '../images/sprite.png',
-        'cssFormat': 'less',
-        'algorithm': 'binary-tree', 
-        'src': ['<%= basePath %>/LVH/_payment/images/*.png'],
-        'destImg': '<%= basePath %>/LVH/_payment/sprite/sprite.png',
-        'destCSS': '<%= basePath %>/LVH/_payment/sprite/sprite.less'
-      }
-    }
-    /*sprite: {
-      mobSite: {
-        src: ls._mobSite().imgSrc(),
-        destImg: ls._mobSite().destImg(),
-        destCSS: ls._mobSite().destCSS(),
-        padding: 2,
-        imgPath: '../images/sprite.png',
-        cssTemplate: 'spritesmith-template.mustache',
-        cssFormat: 'less'
-      }
-    }*/
-  });
-  grunt.loadNpmTasks('grunt-spritesmith');
+  //load helpers
+  var helpers = require('./lib/helpers')(grunt);
+
+  //
   grunt.loadTasks('./tasks');
+  
   grunt.registerTask('default', ['sprite']);
-  grunt.registerTask('LVH', ['sprite:mobSiteLVH', 'sprite:webSiteLVH', 'sprite:paymentLVH']);
+
+  helpers.registerTask('LVH');
 };
