899a01db25a5a9f31fd9261c280b27f2cdbc9420
rybka <rashan_soft@inbox.ru>
2014-11-02 15:13:54 +0200

    add comment

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,11 @@ module.exports = function(grunt) {
   //load helpers
   var helpers = require('./lib/helpers')(grunt);
 
-  //
+  //load tasks
   grunt.loadTasks('./tasks');
-  
+  //register default task
   grunt.registerTask('default', ['sprite']);
-
+  //register custom task
   helpers.registerTask('LVH', ['mobSite', 'webSite', 'payment']);
   helpers.registerTask('SD', ['mobSite', 'webSite']);
   helpers.registerTask('MD', ['payment']);
